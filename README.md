034460fd751e0ae031dc53e2e2b327db759f23e6
ninaargade <argade.nina@gmail.com>
2021-09-19 15:47:24 -0700

    Finished

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# BeaverHacksFall2021
+Beaverhacks Fall2021: Quarterly hackathon with theme focused on climate change/energy
+
+This program reads the attached graph text file to extract the necessary data points of years and corresponding
+global land-ocean temperature indices. The text file contains records of the global land-ocean temperature index for 
+each year since 1880. The temperature index indicates relative changes in temperature during that year. A positive 
+index indicates relative warmth, whereas a negative index indicates relative coolness. This data was obtained from NASA, 
+at the following webpage: https://climate.nasa.gov/vital-signs/global-temperature/. 
+
+After extracting the relevant data, this program then uses the matplotlib  package to generate a graph of the data as 
+a scatter plot with a corresponding trend line which will show that the average global temperature has been rising 
+steadily since 1880, most notably within the last 20-40 years.
--- TrafficTracker.py
+++ TrafficTracker.py
@@ -1,56 +1,46 @@
-# Date: September 2021
+# Author: Nina Argade
+# Date: 9/19/2021
+# Description: This program reads the graph text file to extract the necessary data points of years and corresponding
+# global land-ocean temperature indices. It will then use the matplotlib package to generate a graph of the data as a
+# scatter plot with a corresponding trend line which will show that the average global temperature has been rising
+# steadily since 1880, most notably within the last 20-40 years.
 
-import bs4
-import requests
 
 from matplotlib import pyplot
 
 
-# start = "Imphal"
-# destination = "San Francisco"
-
-# url_car = "https://google.com/search?q=time+to+get+from" + start + "to" + destination + "via+car"
-# url_car = "https://google.com/search?q=weather+in+" + start
-
-# url_car = "https://data.giss.nasa.gov/gistemp/graphs/graph_data/Global_Mean_Estimates_based_on_Land_and_Ocean_Data/graph.txt"
-#
-# request_result_car = requests.get(url_car)
-#
-# print(request_result_car)
-#
-# soup_car = bs4.BeautifulSoup(request_result_car, "html.parser")
-#
-# # time_car = soup_car.find("div", class_='BNeawe').text
-#
-# print(soup_car)
-
 def main():
+    """Main function reads text file containing years and corresponding temperature indices and
+    displays data in a graph."""
+
+    # read file
     file = open("graph.txt", "r")
     lines = file.readlines()
     file.close()
 
-    year_x = []
-    index_y = []
-    smoothing_y = []
+    # create lists of data
+    year_x = []  # year
+    index_y = []  # temperature index
+    smoothing_y = []  # temperature index points to create trend line
 
+    # extract data from each line of txt file
     for index, line in enumerate(lines):
-        print("line {}: {}".format(index, line.split()))
-        if index >= 5:
-            if int(line.split()[0]) % 10 == 0:
-                year_x.append(line.split()[0])
-                index_y.append(float(line.split()[1]))
-                smoothing_y.append(float(line.split()[2]))
+        # print("line {}: {}".format(index, line.split()))
+        if index >= 13:
+            if int(line.split()[0]) % 5 == 0:  # 5 year increments
+                year_x.append(line.split()[0])  # year
+                index_y.append(float(line.split()[1]))  # temperature index
+                smoothing_y.append(float(line.split()[2]))  # temperature index points to create trend line
 
-    print(year_x)
-    print(index_y)
-
-    # plot graph with corresponding sort function times for both series of data points
-    pyplot.plot(year_x, index_y, 'ro--', label='Bubble Sort')
-    pyplot.plot(year_x, smoothing_y, 'go--', linewidth=2, label='Insertion Sort')
+    # plot graph with corresponding temperature indices and trend line
+    pyplot.plot(year_x, index_y, 'ro', label='Annual Mean')  # scatter plot
+    pyplot.plot(year_x, smoothing_y, 'go--', linewidth=2, label='Lowess Smoothing')  # trend line
     pyplot.xlabel("Year")
-    pyplot.ylabel("Temperature Anomaly (C)")
+    pyplot.ylabel("Temperature Anomaly (°C)")
+    pyplot.title("GLOBAL LAND-OCEAN TEMPERATURE INDEX")
     pyplot.legend()
     pyplot.show()
 
 
+# call main function to generate graph
 main()
